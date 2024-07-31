2fe8f4c40c3df87214e2037be1af5b3ef2acaa24
afuentes <alex@glyfo.com>
2024-07-31 03:15:46 +0000

    updated

--- app.vue
+++ app.vue
@@ -1,30 +1,5 @@
-<script setup lang="ts">
-const nav = [
-  { label: 'Home', to: '/' },
-  { label: 'Custom', to: '/custom' },
-  { label: 'Dynamic', to: '/dynamic' },
-  { label: 'Other', to: '/other' },
-]
-</script>
-
 <template>
-  <NuxtExample dir="features/layouts" :nav="nav">
-    <NuxtLayout class="p-4">
+  <NuxtLayout>
       <NuxtPage />
-    </NuxtLayout>
-    <div class="flex items-center gap-4 mt-4 p-2">
-      <UButton color="white" @click="setPageLayout('default')">
-        layouts/default.vue
-      </UButton>
-      <UButton color="white" @click="setPageLayout('custom')">
-        layouts/custom.vue
-      </UButton>
-      <UButton color="white" @click="setPageLayout('other')">
-        layouts/other.vue
-      </UButton>
-      <UButton color="white" @click="setPageLayout('false')">
-        Remove layout
-      </UButton>
-    </div>
-  </NuxtExample>
+  </NuxtLayout>
 </template>
--- layouts/other.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="relative border border-[#e3ca9e] p-4 bg-[#e3cd9e87]">
-      <slot />
-      <span class="absolute top-0 left-1 text-[#b56238] font-mono">layouts/other.vue</span>
-    </div>
-  </template>
