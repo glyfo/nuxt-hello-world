0fe54a5f8f2cb580753ec810c25a19751c870664
afuentes <alex@glyfo.com>
2024-08-03 06:59:16 +0000

    updated

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLayout>
-      <NuxtPage />
+  <NuxtLayout name="default">
+    <NuxtPage />
   </NuxtLayout>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,8 @@
     <nav class="py-2 relative w-full">
       <div class="px-6">
         <NuxtLink to="/">
-          <img class="h-20 w-20 mr-16" src="/logo.svg" alt="Store logo" />
+          <img class="h-20 w-20 mr-16" src="" alt="Store logo" />
         </NuxtLink>
       </div>
     </nav>
-  </template>
   </template>
