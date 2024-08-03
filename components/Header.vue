0c429f24fe759ca395479bd58672a9a09bf72214
afuentes <alex@glyfo.com>
2024-08-03 07:09:51 +0000

    updated

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-    <nav class="py-2 relative w-full">
-      <div class="px-6">
+    <nav class="relative w-full">
+      <div class="px-1">
         <NuxtLink to="/">
-          <img class="h-20 w-20 mr-2" src="/logo400x400.png" alt="Store logo" />
+          <img class="h-12 w-12" src="/logo400x400.png" alt="Store logo" />
         </NuxtLink>
       </div>
     </nav>
